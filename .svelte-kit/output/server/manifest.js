8ba3c80e39bbf41029567350ed34fcecf2253f77
stahlenstein <stahlstradamus@gmail.com>
2023-12-03 16:18:14 -0500

    added build

--- .svelte-kit/output/server/manifest.js
+++ .svelte-kit/output/server/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set([".DS_Store",".nojekyll","Data/images/lct_button.png","favicon.svg"]),
 	mimeTypes: {".png":"image/png",".svg":"image/svg+xml"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.bb33501c.js","app":"_app/immutable/entry/app.1beac503.js","imports":["_app/immutable/entry/start.bb33501c.js","_app/immutable/chunks/scheduler.b108d059.js","_app/immutable/chunks/singletons.59fd9195.js","_app/immutable/chunks/paths.cc00896a.js","_app/immutable/entry/app.1beac503.js","_app/immutable/chunks/preload-helper.41c905a7.js","_app/immutable/chunks/scheduler.b108d059.js","_app/immutable/chunks/index.bb499846.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.4454912b.js","app":"_app/immutable/entry/app.c950eee1.js","imports":["_app/immutable/entry/start.4454912b.js","_app/immutable/chunks/scheduler.b108d059.js","_app/immutable/chunks/singletons.c44ad2d6.js","_app/immutable/chunks/paths.3ac990d4.js","_app/immutable/entry/app.c950eee1.js","_app/immutable/chunks/preload-helper.41c905a7.js","_app/immutable/chunks/scheduler.b108d059.js","_app/immutable/chunks/index.bb499846.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js'))
--- .svelte-kit/output/server/nodes/2.js
+++ .svelte-kit/output/server/nodes/2.js
@@ -5,6 +5,6 @@ let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
 export { server };
 export const server_id = "src/routes/+page.server.js";
-export const imports = ["_app/immutable/nodes/2.a06215ab.js","_app/immutable/chunks/2.4c186883.js","_app/immutable/chunks/scheduler.b108d059.js","_app/immutable/chunks/index.bb499846.js","_app/immutable/chunks/preload-helper.41c905a7.js","_app/immutable/chunks/paths.cc00896a.js"];
+export const imports = ["_app/immutable/nodes/2.b63660f5.js","_app/immutable/chunks/2.d9ceea0c.js","_app/immutable/chunks/scheduler.b108d059.js","_app/immutable/chunks/index.bb499846.js","_app/immutable/chunks/preload-helper.41c905a7.js","_app/immutable/chunks/paths.3ac990d4.js"];
 export const stylesheets = ["_app/immutable/assets/2.c298b839.css"];
 export const fonts = [];
