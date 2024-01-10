d9302a6e55786dcccd3345b66c8b67bc8637de39
jacob <stahlnypl@icloud.com>
2024-01-10 16:51:11 -0500

    icon update

--- .svelte-kit/output/server/manifest-full.js
+++ .svelte-kit/output/server/manifest-full.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set([".DS_Store",".nojekyll","Data/.DS_Store","Data/images/lct_button.png","favicon.svg","favicon_152.png","favicon_167.png","favicon_180.png","touch-icon-iphone.png"]),
 	mimeTypes: {".png":"image/png",".svg":"image/svg+xml"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.5b630c9b.js","app":"_app/immutable/entry/app.976840ce.js","imports":["_app/immutable/entry/start.5b630c9b.js","_app/immutable/chunks/scheduler.b108d059.js","_app/immutable/chunks/singletons.da1aab63.js","_app/immutable/chunks/paths.a58b944c.js","_app/immutable/entry/app.976840ce.js","_app/immutable/chunks/preload-helper.41c905a7.js","_app/immutable/chunks/scheduler.b108d059.js","_app/immutable/chunks/index.bb499846.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.ab361382.js","app":"_app/immutable/entry/app.00c1d1d3.js","imports":["_app/immutable/entry/start.ab361382.js","_app/immutable/chunks/scheduler.b108d059.js","_app/immutable/chunks/singletons.0d199ac1.js","_app/immutable/chunks/paths.ab9bc484.js","_app/immutable/entry/app.00c1d1d3.js","_app/immutable/chunks/preload-helper.41c905a7.js","_app/immutable/chunks/scheduler.b108d059.js","_app/immutable/chunks/index.bb499846.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
